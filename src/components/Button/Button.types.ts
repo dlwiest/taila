4a330b3307b55ee2039110eeafffdc56878bc83b
Derrick Wiest <me@dlwiest.com>
2024-01-11 13:51:21 -0500

    Added storybook

--- src/components/Button/Button.types.ts
+++
@@ -1,4 +0,0 @@
-import { ButtonHTMLAttributes } from "react";
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  size?: 'small' | 'medium' | 'large';
-}
---
+++ src/stories/button.stories.tsx
@@ -0,0 +1,8 @@
+import { Button } from '..';
+
+export default {
+    title: 'Components/Button',
+    component: Button,
+}
+
+export const Primary = () => <Button />
